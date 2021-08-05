9de0a6ebbe53d1d6525738c32d4ba2804f85d013
Aghata Sophia <aghata@upplify.com>
2021-08-05 17:51:51 -0300

    Create Auth Module and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/signin', pathMatch: 'full' },
+    { path: 'signin', component: SignInComponent },
+    {
+        path: 'dashboard',
+        canActivate: [AuthGuard],
+        loadChildren: async () => (await import('./dashboard/dashboard.module')).DashboardModule,
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
