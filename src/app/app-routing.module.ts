82841a6e1fcc470f2c27b63278cc3ea6a2dae9fd
Rafael A <rafaelaraujo.02@hotmail.com>
2021-08-08 13:10:45 -0300

    add signup route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/signin', pathMatch: 'full' },
     { path: 'signin', component: SignInComponent },
+    { path: 'signup', component: SignUpComponent },
     {
         path: 'dashboard',
         canActivate: [AuthGuard],
