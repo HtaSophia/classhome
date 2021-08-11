397b248b8222f16a34801bb30b55dcbbe9520caf
Rafael A <rafaelaraujo.02@hotmail.com>
2021-08-11 00:48:04 -0300

    Remoção de código

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 // eslint-disable-next-line import/no-unresolved
-import { AccountService } from 'src/app/account/account.service';
-import { SubSink } from 'subsink';
 
 @Component({
     selector: 'app-forgot-password',
@@ -13,37 +10,9 @@ import { SubSink } from 'subsink';
 export class ForgotPasswordComponent implements OnInit {
     public forgotPasswordForm: FormGroup;
 
-    public isLoading = false;
-
-    private subscriptions = new SubSink();
-
-    constructor(
-        private readonly formBuilder: FormBuilder,
-        private readonly router: Router,
-        private readonly accountService: AccountService,
-    ) {}
-
     public ngOnInit(): void {
         this.createForm();
     }
 
-    public ngOnDestroy(): void {
-        this.subscriptions.unsubscribe();
-    }
-
-    public onSubmit(): void {
-        this.isLoading = true;
-
-        this.subscriptions.sink = this.accountService.signin(this.forgotPasswordForm.value).subscribe((_response) => {
-            this.isLoading = false;
-
-            void this.router.navigate(['dashboard']);
-        });
-    }
-
-    private createForm(): void {
-        this.forgotPasswordForm = this.formBuilder.group({
-            email: ['', Validators.compose([Validators.required, Validators.email])],
-        });
-    }
+    private createForm(): void {}
 }
