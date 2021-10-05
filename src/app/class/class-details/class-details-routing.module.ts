8ba63caf3af5be7b24341b3e9d2860b3307ffdd3
Rafael A <rafaelaraujo.02@hotmail.com>
2021-10-05 14:29:15 -0300

    Add path people in class-details-routing

--- src/app/class/class-details/class-details-routing.module.ts
+++ src/app/class/class-details/class-details-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoggedInGuard } from '../../auth/guards/logged-in.guard';
 import { ClassDetailsComponent } from './class-details.component';
 import { PeopleComponent } from './people/people.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/people', pathMatch: 'full' },
-    { path: 'people', canActivate: [LoggedInGuard], component: PeopleComponent },
     {
         path: '',
         component: ClassDetailsComponent,
         children: [
+            { path: 'people', component: PeopleComponent },
             { path: '', redirectTo: 'mural', pathMatch: 'full' },
             {
                 path: 'mural',
